85ad2367da401e3961117e3dcdde8abf8ca399b4
Winston Loh <winsobaggins@yahoo.com.sg>
2020-05-16 07:10:10 +0800

    mocks up lifestyle/discover page

--- pwa/src/app/lifestyle/lifestyle.module.ts
+++ pwa/src/app/lifestyle/lifestyle.module.ts
@@ -3,13 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { LifestyleRoutingModule } from './lifestyle-routing.module';
 import { LifestyleComponent } from './lifestyle.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
   declarations: [LifestyleComponent],
   imports: [
     CommonModule,
-    LifestyleRoutingModule
+    LifestyleRoutingModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
   ]
 })
 export class LifestyleModule { }
