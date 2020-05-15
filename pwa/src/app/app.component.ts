8d1892b3ed6f66fd066b95ccc5faebfb3a05e4eb
Winston Loh <winsobaggins@yahoo.com.sg>
2020-05-15 11:27:06 +0800

    adds non-pwa parallax-scrolling, removes express

--- pwa/src/app/app.component.ts
+++ pwa/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor() { }
+  constructor(public router:Router) { }
   
   title = "TOΛZTΞR";
   showFiller = false;
   opened = true;
   isDarkTheme = true;
+  nonPwaPages = ["/landing"];
 
 }
--- pwa/src/app/landing/landing.component.ts
+++ pwa/src/app/landing/landing.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+// import { universalParallax } from 'universal-parallax';
+declare var universalParallax: any;
+// console.log(universalParallax);
+// import * as up from 'universalParallax';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  up:any;
+
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.up = new universalParallax().init({
+      speed: 4
+    });
 
+  }
 }
